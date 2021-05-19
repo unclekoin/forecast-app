f184dbc8e2e8480a62f2ec542083c53ce4d2e5b3
Pavel Koryakin <unclekoin@gmail.com>
2021-05-19 17:53:58 +0300

    feat: refactoring code

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ const request= require('postman-request');
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=3f797cf64bd47aee79bfdcaba83e6ea2&query=${ latitude },${ longitude }&units=m`;
 
-  request({url, json: true}, (error, response) => {
+  request({url, json: true}, (error, { body } = {}) => {
     if (error) {
       callback('Unable to connect to weather service!', undefined)
-    } else if (response.body.error) {
-      callback(response.body.error.info, undefined)
+    } else if (body.error) {
+      callback(body.error.info, undefined)
     } else {
-      const data = response.body.current;
+      const data = body.current;
       const info = `${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`
       callback(undefined, info)
     }
